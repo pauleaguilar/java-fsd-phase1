eaead0392c29e335d248f3c9dc2a2e1491a6cfc5
Paul Aguilar <pauleaguilar@gmail.com>
2021-02-08 10:31:34 -0600

    "update"

--- FileHandling/src/FileHandling.java
+++ FileHandling/src/FileHandling.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class FileHandling {
+import java.io.File;
+import java.util.Scanner;
+public class FileHandling {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("File test.txt will be created.");
+        String fileName= "test.txt";
+        System.out.println("Enter text to write to file: ");
+        String scanText = scan.nextLine();
+        scan.close();
+
+        CreateFile createFile = new CreateFile(fileName);
+        File newFile = createFile.createaFile();
+
+        WriteFile w = new WriteFile(scanText, newFile);
+        w.Write();
+        w.append();
+    }
+
 }
--- FileHandling/src/WriteFile.java
+++ FileHandling/src/WriteFile.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class WriteFile {
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+
+public class WriteFile {
+
+
+    FileWriter write;
+    private String textToWrite;
+    File myFile;
+
+    public WriteFile(String textToWrite, File myFile) {
+        this.textToWrite = textToWrite;
+        this.myFile = myFile;
+    }
+
+
+    public void Write() {
+
+        try {
+            write = new FileWriter(myFile);
+            write.write(textToWrite);
+            System.out.println("Successfully wrote to the file");
+        }
+        catch (IOException iex) {
+            System.err.println("failed writing to file");
+
+        }
+        catch (NullPointerException npe) {
+            System.err.println("File does not exist yet.");
+
+        }
+        finally {
+            try {
+                if (write != null) {
+                    write.flush();
+                    write.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void append() {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(myFile, true); //the true will append the new data
+            fw.write("\nthis text has been appended to the file\n");//appends the string to the file
+
+        }
+        catch (IOException e) {
+            System.err.println(e);
+        }
+        finally {
+            try {
+                if (fw != null) {
+                    fw.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
